4ace46971f5d96dbe1d89f2ab1847a40226ec1e5
anunirmani <anuttaranirmani@gmail.com>
2025-09-22 16:39:07 +0530

    Login page completed

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,82 +1,102 @@
 import { useState } from "react";
 import { FaFacebook, FaGoogle } from "react-icons/fa";
 import { FaRegEye, FaRegEyeSlash } from "react-icons/fa";
-import "../css/login.css"; // ✅ CSS import
+import logoImage from "../assets/logo.png";
+import "../css/login.css";
 
 function Login() {
   const [showPassword, setShowPassword] = useState(false);
 
   return (
     <div className="login-page">
-      <div className="login-box">
-        {/* Heading */}
-        <h2 className="login-title">Nice To See You Again</h2>
-        <p className="login-subtitle">Log in to your account</p>
-
-        {/* Logo */}
-        <div className="login-logo">
-          <img src="src/assets/logo.png" alt="Movie Express" />
-        </div>
-
-        {/* Form */}
-        <form className="login-form">
-          {/* Email */}
-          <div className="form-group">
-            <label>Email</label>
-            <input type="email" placeholder="jone@gmail.com" />
+      <div className="login-container">
+        <div className="login-box">
+          {/* Header */}
+          <div className="login-header">
+            <h1 className="login-title">Nice To See You Again</h1>
+            <p className="login-subtitle">Log in to your account</p>
           </div>
 
-          {/* Password */}
-          <div className="form-group">
-            <label>Password</label>
-            <div className="password-wrapper">
-              <input
-                type={showPassword ? "text" : "password"}
-                placeholder="******"
-              />
-              <button
-                type="button"
-                className="toggle-password"
-                onClick={() => setShowPassword(!showPassword)}
-              >
-                {showPassword ? <FaRegEyeSlash /> : <FaRegEye />}
-              </button>
+          {/* Logo */}
+          <div className="logo-section">
+            <div className="logo-image">
+              <img src={logoImage} alt="Movie Express Logo" />
             </div>
+            <div className="logo-text">MOVIE EXPRESS</div>
           </div>
 
-          {/* Remember + Forget */}
-          <div className="form-options">
-            <label>
-              <input type="checkbox" /> Remember Me
-            </label>
-            <a href="#" className="forgot-link">
-              Forget Password?
-            </a>
+          {/* Form */}
+          <form className="login-form">
+            {/* Email Field */}
+            <div className="form-group">
+              <label className="form-label">Email</label>
+              <input 
+                type="email" 
+                className="form-input" 
+                placeholder="jone@gmail.com" 
+                defaultValue="jone@gmail.com"
+              />
+            </div>
+
+            {/* Password Field */}
+            <div className="form-group">
+              <label className="form-label">Password</label>
+              <div className="password-input-wrapper">
+                <input
+                  type={showPassword ? "text" : "password"}
+                  className="form-input password-input"
+                  placeholder="******"
+                  // defaultValue="******"
+                />
+                <button
+                  type="button"
+                  className="password-toggle"
+                  onClick={() => setShowPassword(!showPassword)}
+                >
+                  {showPassword ? <FaRegEyeSlash /> : <FaRegEye />}
+                </button>
+              </div>
+            </div>
+
+            {/* Options Row */}
+            <div className="form-options">
+              <label className="checkbox-container">
+                <input type="checkbox" className="checkbox" />
+                <span className="checkbox-label">Remember Me</span>
+              </label>
+              <a href="#" className="forgot-password">Forget Password?</a>
+            </div>
+
+            {/* Login Button */}
+            <button type="submit" className="login-button">
+              Login
+            </button>
+          </form>
+
+          {/* Divider */}
+          <div className="divider">
+            <span className="divider-text">or</span>
           </div>
 
-          {/* Login Button */}
-          <button type="submit" className="login-btn">
-            Login
-          </button>
-        </form>
+          {/* Social Login Buttons */}
+          <div className="social-buttons">
+            <button className="social-button facebook-button">
+              <FaFacebook className="social-icon facebook-icon" />
+              <span>Facebook</span>
+            </button>
+            <button className="social-button google-button">
+              <FaGoogle className="social-icon google-icon" />
+              <span>Google</span>
+            </button>
+          </div>
 
-        {/* Divider */}
-        <div className="divider">or</div>
-
-        {/* Social Login */}
-        <div className="social-login">
-          <button className="social-btn facebook">
-            <FaFacebook /> Facebook
-          </button>
-          <button className="social-btn google">
-            <FaGoogle /> Google
-          </button>
+          {/* Sign Up Link */}
+          <div className="signup-section">
+            <p className="signup-text">
+              Don't have an account? <a href="#" className="signup-link">Sign Up</a>
+            </p>
+          </div>
         </div>
-
-        {/* Sign up link */}
-        <p className="signup-text">
-          Don’t have an account? <a href="#">Sign Up</a>
-        </p>
       </div>
     </div>
   );
